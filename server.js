532a0af36113fa76bf4f4366c2752a8c617cfd70
Patryk360 <nexigamingyt@gmail.com>
2026-09-23 09:28:41 +0200

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const mime = require('mime-types');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 const mediaDir = path.join(__dirname, 'media');
 
 app.use(express.static(path.join(__dirname, 'public')));
